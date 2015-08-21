2e6c827369a6516931f383ceabc3e6cab032af95
bsevansunc <bsevans.unc@gmail.com>
2015-08-21 13:07:06 -0400

    added worksheet script for intro to r

---
+++ scripts/IntroToR_worksheet.R
@@ -0,0 +1,169 @@
+############################################################################*
+# ----  WORKSHEET: PRELIMINARY R LAB  ----
+############################################################################*
+# TITLE: Introduction to R: The structure of R
+# COURSE: The Ecology and Conservation of Migratory Birds 2015
+# AUTHOR: Brian S. Evans
+
+# Instructions for students:
+# Below each prompt, enter the CODE you used to address each objective.
+# Please DO NOT provide the answers to the questions.
+
+# ==========================================================================*
+# ---- Exercise One ----
+# ==========================================================================*
+
+# Write the code for the [provided] mathematical expression in R and assign
+# a name to the object:
+
+
+
+# ==========================================================================*
+# ---- Exercise Two ----
+# ==========================================================================*
+
+# 1. Create an object that contains the integers 1,1, and 2. 
+
+
+
+# 2. Assign the name "gen1" to the vector and use the "sum" function to add
+# these values together.
+
+
+
+# ==========================================================================*
+# ---- Exercise Three ----
+# ==========================================================================*
+
+# Create a single R object that includes each of the [provided] three
+# objects:
+
+
+# ==========================================================================*
+# ---- Exercise Four ----
+# ==========================================================================*
+
+# 1. Extract the fifth value (24) of the rabbits vector.
+
+
+
+# 2. Use the ":" symbol to extract the third through fifth values in the 
+# rabbits vector.
+
+
+
+# 3. Use the combine function to extract the third through fifth values in
+# the rabbits vector.
+
+
+
+# 4. Use the combine and length functions to extract the third and last
+# values in the rabbits vector.
+
+
+
+# ==========================================================================*
+# ---- Exercise Five ----
+# ==========================================================================*
+
+# 1. Extract the value at the cell address associated with row two, column 
+# two in dataframe1.
+
+
+
+# 2. Extract the vector of values associated with row one of dataframe1.
+
+
+
+# 3. Extract the vector of values associated with column two of dataframe1.
+
+
+
+# 4. Use the nrow and ncol functions to extract the last row and column of
+# dataframe1.
+
+
+
+# 5. Use the ":" symbol to extract the first and second rows of matrix1.
+
+
+
+# 6. Use the combine function to extract the second and third rows of 
+# dataframe1.
+
+
+
+
+# ==========================================================================*
+# ---- Exercise Six ----
+# ==========================================================================*
+
+# Run the [provided script to read exampleBirdData.csv into R.
+           
+# 1. Determine the class of the birdCounts dataset.
+
+
+
+# 2. Determine the class of the species field within birdCounts.
+
+
+
+# 3. Extract the count field of birdCounts as a vector.
+
+
+
+# 4. Create a data frame of birdCounts consisting of the first 20 records
+# and assign the name "birdCounts2".
+
+
+
+# 5. Change the name of the counts field in birdCounts2 to "abundance".
+
+
+
+# 6. Subset birdCount2 to the fields "site",species", "abundance", and
+# "diet". 
+
+
+
+# 7. Change the count for Gray Catbirds (grca) at site 3 from 1 to 2 using 
+# the address of the value.
+
+
+
+# ==========================================================================*
+# ---- Exercise Seven ----
+# ==========================================================================*
+
+# For the birdCount dataset, address the following:
+
+# 1. Extract the last 10 records using the row addresses (do not use the
+# "tail" function).
+
+
+
+# 2. What are the factor levels for the column "foraging"?
+
+
+
+# 3. How many unique species does the dataset contain?
+
+
+
+# 4. What is the mean value of counts?
+
+
+
+# ==========================================================================*
+# ---- Exercise Eight ----
+# ==========================================================================*
+
+# 1. How many species are insectivores?
+
+
+
+# 2. In what proportion of sites were insectivores observed?
+
+
+
+#### END ####
