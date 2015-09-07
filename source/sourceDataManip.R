a743f619f823944db655247675830ca98f11b42f
bsevansunc <bsevans.unc@gmail.com>
2015-09-07 11:34:34 -0400

    fixed column assignment on wideFrame

--- source/sourceDataManip.R
+++ source/sourceDataManip.R
@@ -8,7 +8,7 @@ untidyFrame <- data.frame(subject = c('A','B','C'),
   treatmentA = c(1.3, 2.3, 3.1), 
   treatmentB = c(2.9, 3.2, 4.6))
 
-wideFrame <- data.frame(species <- c('amro', 'carw', 'grca'),
+wideFrame <- data.frame(species = c('amro', 'carw', 'grca'),
   d10 = c(0,0,1),
   d20 = c(1,1,0), 
   d30 = c(0,0,0))
