562e58e371fe34609eddf4ea2cf5b112b14204eb
bsevansunc <bsevans.unc@gmail.com>
2015-09-07 11:46:27 -0400

    removed wide and long counts

--- source/sourceDataManip.R
+++ source/sourceDataManip.R
@@ -1,8 +1,6 @@
 library(dplyr) ; library(tidyr)
 
 birdCountsURL <- getURL('https://raw.githubusercontent.com/SCBI-MigBirds/MigBirds/master/data/exampleBirdData.csv')
-wideURL <- getURL('https://raw.githubusercontent.com/SCBI-MigBirds/MigBirds/master/data/wideCounts.csv')
-longURL <- getURL('https://raw.githubusercontent.com/SCBI-MigBirds/MigBirds/master/data/longCounts.csv')
 
 untidyFrame <- data.frame(subject = c('A','B','C'), 
   treatmentA = c(1.3, 2.3, 3.1), 
@@ -17,11 +15,8 @@ longFrame <- data.frame(site = c('site1', 'site2' ,'site2',rep('site3',3)),
   species = c('amro', c('carw','grca'), 'amro','carw','grca'),
   count = c(1,1,2, 5, 1, 2))
                      
-
-
 birdCounts <- read.csv(text = birdCountsURL)
-wideCounts <- read.csv(text = wideURL)
-longCounts <- read.csv(text = longURL)
+
 
 
 
