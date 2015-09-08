e99d9277537cf2aadae0c0fce7b7e09ac28d8841
bsevansunc <bsevans.unc@gmail.com>
2015-09-08 14:36:31 -0400

    added source file

---
+++ source/sourceStructureIndexLogic.R
@@ -0,0 +1,17 @@
+library(dplyr) ; library(tidyr)
+
+birdCountsURL <- getURL('https://raw.githubusercontent.com/SCBI-MigBirds/MigBirds/master/data/exampleBirdData.csv')
+
+birdCounts <- read.csv(text = birdCountsURL) %>%
+  tbl_df()
+
+exampleFrame <- data.frame(site = c('site1', 'site2' ,'site2',rep('site3',3)),
+  species = c('amro', c('carw','grca'), 'amro','carw','grca'),
+  count = c(1,1,2, 5, 1, 2))
+                     
+
+
+
+
+
+
