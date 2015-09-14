01faeef280371c5bf141c8cae3992def536edccf
Brian S Evans <bsevans.unc@gmail.com>
2015-09-14 16:05:03 -0400

    removed dplry

--- source/sourceStructureIndexLogic.R
+++ source/sourceStructureIndexLogic.R
@@ -1,5 +1,3 @@
-library(dplyr) ; library(tidyr)
-
 birdCountsURL <- getURL('https://raw.githubusercontent.com/SCBI-MigBirds/MigBirds/master/data/exampleBirdData.csv')
 
 birdCounts <- read.csv(text = birdCountsURL)
@@ -10,6 +8,9 @@ exampleData <- data.frame(site = c('site1', 'site2' ,'site2',rep('site3',3)),
 
 exampleData2 <- matrix(c(1,1,2, 3, 5,8), nrow = 3, ncol = 2)
 
+birdCounts
+exampleData
+exampleData2
 
 
 
