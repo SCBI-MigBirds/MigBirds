7b4deeccb5250cf4af685ac0c8ed84a7a8d93d3c
bsevansunc <bsevans.unc@gmail.com>
2015-09-15 11:32:06 -0400

    removed first install packages

--- source/sourcePackageInstall.R
+++ source/sourcePackageInstall.R
@@ -2,5 +2,4 @@
 
 packages <- c("ggplot2", "RCurl","dplyr", "tidyr","raster")
 packagesToInstall <- packages[!(packages %in% installed.packages()[,"Package"])]
-install.packages(packagesToInstall)
 if(length(packagesToInstall)>0) {install.packages(packagesToInstall)}
