9585005e92189884e01268a4291eee9beb2ed71f
bsevansunc <bsevans.unc@gmail.com>
2015-09-15 22:44:35 -0400

    removed internal install packages

--- source/sourceDataManip.R
+++ source/sourceDataManip.R
@@ -1,6 +1,5 @@
 packages <- c("ggplot2", "RCurl","dplyr", "tidyr","raster")
 packagesToInstall <- packages[!(packages %in% installed.packages()[,"Package"])]
-install.packages(packagesToInstall)
 if(length(packagesToInstall)>0) {install.packages(packagesToInstall)}
 
 library(dplyr) ; library(tidyr)
